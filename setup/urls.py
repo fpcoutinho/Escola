be7b1bdc62c70ffc43b772015d2bacbdaccb791a
fpcoutinho <fpcoutinho5@gmail.com>
2023-11-05 14:32:30 -0300

    curso 3, versionamento de api

--- setup/urls.py
+++ setup/urls.py
@@ -1,18 +1,16 @@
 from django.contrib import admin
-from django.urls import path
-from django.urls import include
-from escola.views import AlunosViewSet, CursosViewSet, MatriculasViewSet, ListaMatriculasAluno, ListaAlunosMatriculados
+from django.urls import path,include
+from escola.views import AlunosViewSet, CursosViewSet, MatriculaViewSet, ListaMatriculasAluno, ListaAlunosMatriculados
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register('alunos', AlunosViewSet, basename='Alunos')
 router.register('cursos', CursosViewSet, basename='Cursos')
-router.register('matriculas', MatriculasViewSet, basename='Matriculas')
+router.register('matriculas', MatriculaViewSet, basename='Matriculas')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
-    path('alunos/<int:pk>/cursos/', ListaMatriculasAluno.as_view()),
-    path('cursos/<int:pk>/alunos/', ListaAlunosMatriculados.as_view()),
-    path('auth/', include('rest_framework.urls')),
+    path('', include(router.urls) ),
+    path('aluno/<int:pk>/matriculas/', ListaMatriculasAluno.as_view()),
+    path('curso/<int:pk>/matriculas/', ListaAlunosMatriculados.as_view())
 ]
